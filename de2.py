589e6b55f98d3c4cc8757c7fc718b10af0175234
Breno S <breno.silva2004@hotmail.com>
2025-06-01 00:01:23 -0300

    Remover o argumento desnecessario no script e colocar mais info

--- de2.py
+++ de2.py
@@ -2,35 +2,35 @@ import serial
 import time
 import sys
 
-if len(sys.argv) != 4:
-    print(f"Uso: {sys.argv[0]} <porta> <baud> <await>")
+if len(sys.argv) != 3:
+    print(f"Uso: {sys.argv[0]} <porta> <baud>")
     exit()
 
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere eros in metus molestie venenatis. Aliquam commodo augue a facilisis posuere. Suspendisse vitae ipsum sollicitudin, ornare arcu vel, viverra magna. Fusce eu justo lectus. Proin volutpat."
 
 baudrate = int(sys.argv[2])
-wait = int(sys.argv[3])
+# inicializa a porta serial para comunicacao 
+# para linux, a porta sera /dev/ttyUSBx enquanto para windows, a porta sera COMx
+# sendo x um numero que pode ser visto, respectivamente, rodando `ls /dev/ttyUSB*`, ou no Gerenciador de Dispositivos na secao "Portas (COM e LPT)" 
 placa = serial.Serial(sys.argv[1], baudrate)
 
+# tempo inicial
 start = time.time()
-#for caractere in lorem:
-#    placa.write(caractere.encode())
-#    time.sleep(wait/(baudrate))
 placa.write(lorem.encode())
-#time.sleep(wait/(baudrate))
+# o caractere 0x00 sinaliza a placa que a transmissao acabou e deve enviar toda a string de volta
 placa.write(b'\x00')
 
-enviostamp = time.time()
-envio = enviostamp - start
+# tempo apos enviar os dados e antes de receber de volta
+envioStamp = time.time()
+tempoEnvio = enviostamp - start
 
-#while placa.in_waiting < 256:
-#    time.sleep(wait/(200000*baudrate))
-
-#rec = placa.read_all()
 rec = placa.read(256)
-recebimento = time.time() - enviostamp
+tempoRecebimento = time.time() - enviostamp
+
+# confirmar se o dado recebido eh exatamente igual ao enviado
 print(rec)
 print(rec == lorem.encode())
+
 print("tempo total: ", time.time() - start)
-print("tempo envio: ", envio)
-print("tempo receb: ", recebimento)
+print("tempo envio: ", tempoEnvio)
+print("tempo receb: ", tempoRecebimento)
